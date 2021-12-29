f327fb7cb569a421b4075ce9c606b6899b5ff47b
Sam Komesarook <47465976+SamKomesarook@users.noreply.github.com>
2021-12-29 22:03:52 +1100

    Time: 96 ms (72.47%), Space: 44.8 MB (98.33%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,28 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (root == null) {
+        return root;
+    }
+    if (root.left !== null) {
+        root.left.next = root.right;
+    }
+    if (root.right !== null && root.next !== null) {
+        root.right.next = root.next.left;
+    }
+    connect(root.left);
+    connect(root.right);
+    return root;
+};
