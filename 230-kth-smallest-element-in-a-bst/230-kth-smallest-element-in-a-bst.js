31a02d49795d2aeb89a7a6b026a777bad413410d
Sam Komesarook <47465976+SamKomesarook@users.noreply.github.com>
2022-01-23 15:59:10 +1100

    Time: 154 ms (13.61%), Space: 47.1 MB (8.44%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let arr = makeList(root)
+    return arr[k-1]
+};
+
+let makeList = (node) => {
+    if(node == null){
+        return []
+    }
+    return [...makeList(node.left),node.val,...makeList(node.right)]
+}
